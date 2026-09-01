06683421d4fee31b2675b70f9a4726723f604d62
Dilawar Khan Nasiri <dilawarkhannasiri2021@gmail.com>
2026-09-01 11:18:18 +0430

    Add files via upload

---
+++ task_1.php
@@ -0,0 +1,15 @@
+<?php
+
+// Task 1
+// Full Name: Your Full Name
+// Student ID: Your Student ID
+
+class Library
+{
+    // This is a constant because the maximum number of books should not change.
+    const MAX_BOOKS = 3;
+}
+
+echo "Maximum books allowed: " . Library::MAX_BOOKS;
+
+?>
