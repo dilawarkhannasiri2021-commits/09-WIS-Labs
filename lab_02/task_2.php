cb1fe5226697ae76355736485fa389aa0bff2418
Dilawar Khan Nasiri <dilawarkhannasiri2021@gmail.com>
2026-09-01 11:22:55 +0430

    Add files via upload

---
+++ lab_02/task_2.php
@@ -0,0 +1,23 @@
+<?php
+
+// Task 2
+// Full Name: Your Full Name
+// Student ID: Your Student ID
+
+class StudentCounter
+{
+    public static $count = 0;
+
+    public static function addStudent()
+    {
+        self::$count++;
+    }
+}
+
+StudentCounter::addStudent();
+StudentCounter::addStudent();
+StudentCounter::addStudent();
+
+echo "Total students: " . StudentCounter::$count;
+
+?>
